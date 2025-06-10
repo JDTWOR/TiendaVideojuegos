59ac77c8e50fcf6cdba37beff9a45bb3dd791974
Jhon Deivid Rojas Rodriguez <jhonrojas253@gmail.com>
2025-06-10 10:08:53 -0500

    Completar la clase CLiente

--- source/uniandes/cupi2/videotienda/mundo/Cliente.java
+++ source/uniandes/cupi2/videotienda/mundo/Cliente.java
@@ -1,5 +1,7 @@
 package uniandes.cupi2.videotienda.mundo;
 
+import java.util.ArrayList;
+
 public class Cliente {
 
   private String cedula;
@@ -10,4 +12,52 @@ public class Cliente {
 
   private int saldo;
 
+  public Cliente(String laCedula, String elNombre, String laDireccion) {
+    cedula = laCedula;
+    nombre = elNombre;
+    direccion = laDireccion;
+    saldo = 0;
+  }
+
+  public String darCedula() {
+    return cedula;
+  }
+
+  public int darSaldo() {
+    return saldo;
+  }
+
+  public String darNombre() {
+    return nombre;
+  }
+
+  public String darDireccion() {
+    return direccion;
+  }
+
+  public void alquilarCopia() {
+    System.out.println("Toca hacer el metodo alquilarCopia");
+  }
+
+  public void cargarSaldo(int monto) {
+    saldo += monto;
+  }
+
+  public void descargarSaldo(int monto) {
+    saldo -= monto;
+  }
+
+  public int darNumeroAlquiladas() {
+    return 1;
+  }
+
+  // ArrayList darAlquiladas()
+  //
+  public Copia buscarPeliculaAlquilada(String pelicula, int codigo) {
+    return copia;
+  }
+
+  public void develoverCopia(String pelicula, int codigo) {
+    System.out.println("Toca hacer el metodo develoverCopia");
+  }
 }
